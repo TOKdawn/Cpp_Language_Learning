7d6337e8e5edfbd73643b737d5e1a168a9278c6e
Dawn inShadow <zpy18746442232@gmail.com>
2016-05-10 14:24:16 +0800

    更改

--- 基础语法/函数构造和析构/Stcok.h
+++ 基础语法/函数构造和析构/Stcok.h
@@ -1,22 +1,23 @@
 #ifndef STCOK_H_
 #define STCOK_H_
 
-class Stock{
-    private:
-        char company[30];
-        int shares;
-        double share_val;
-        double total_val;
-        void set_tot(){total_val = shares * share_val;}//可以直接在类声明里写实现
-    public:
-        Stock();//默认构造函数
-        Stock(const char * co,int n=0,double pr=0.0);
-        //构造函数没有返回值,并且要于类名相同
-        ~Stock(){}//析构函数
-        void buy(int num,double price);
-        void sell(int num,double price);
-        void update(double price);
-        void show()const;//this指针
-        const Stock & topval(const Stock & s)const;
-    };
+class Stock {
+private:
+  char company[30];
+  int shares;
+  double share_val;
+  double total_val;
+  void set_tot() { total_val = shares * share_val; } //可以直接在类声明里写实现
+
+public:
+  Stock(); //默认构造函数?
+  Stock(const char *co, int n = 0, double pr = 0.0);
+  //构造函数没有返回值?,并且要于类名相同
+  ~Stock() {} //析构函数
+  void buy(int num, double price);
+  void sell(int num, double price);
+  void update(double price);
+  void show() const; // this指针
+  const Stock &topval(const Stock &s) const;
+};
 #endif
--- 基础语法/函数构造和析构/Stock.cpp
+++ 基础语法/函数构造和析构/Stock.cpp
@@ -1,62 +1,58 @@
-#include <iostream>
 #include "Stock.h"
-Stock::Stock(){
-    std::strcpy(company,"no name");
+#include <iostream>
+Stock::Stock() {
+  std::strcpy(company, "no name");
+  shares = 0;
+  share_val = 0.0;
+  total_val = 0.0;
+} //默认构造函数实现
+Stock::Stock(const char *co, int n, double pr) {
+  std::strncpy(company, co, 29);
+  company[29] = '\0';
+  if (n < 0) {
+    std::cerr << "Number of share can't be negative." << company
+              << "shares set to 0\n";
     shares = 0;
-    share_val = 0.0;
-    total_val = 0.0;
-}//默认构造函数实现
-Stock::Stock(const char * co,int n,double pr){
-    std::strncpy(company,co,29);
-    company[29] = '\0';
-    if(n<0){
-        std::cerr<<"Number of share can't be negative."<<company<<"shares set to 0\n";
-        shares = 0;
-    }
-    else
-        shares = n;
-    share_val = pr;
-    set_tot();
+  } else
+    shares = n;
+  share_val = pr;
+  set_tot();
 }
-//Stock::~Stock(){}
-void Stock::buy(int num,double price){
-    if(num<0){
-        std::cerr<<"错误\n";
-    }
-    else{
-        shares += num;
-        share_val = price;
-        set_tot();
-    }
-}
-void Stock::sell(int num,double price){
-    if(num<0){
-        std::cerr<<"错误\n";
-    }
-    else if(num>shares){
-        std::cerr<<"错误\n";
-    }
-    else{
-        shares -= num;
-        share_val = price;
-        set_tot();
-    }
-}
-void Stock::update(double price){
+// Stock::~Stock(){}
+void Stock::buy(int num, double price) {
+  if (num < 0) {
+    std::cerr << "错误\n";
+  } else {
+    shares += num;
     share_val = price;
     set_tot();
+  }
 }
-void Stock::show()const{
-    using std::cout;
-    using std::endl;
-    cout<<"Company:"<<company
-    <<"Shera:"<<shares<<endl
-    <<"Shera Price: $"<<share_val
-    <<"Total Worth: $"<<total_val<<endl;
+void Stock::sell(int num, double price) {
+  if (num < 0) {
+    std::cerr << "错误\n";
+  } else if (num > shares) {
+    std::cerr << "错误\n";
+  } else {
+    shares -= num;
+    share_val = price;
+    set_tot();
+  }
 }
-const Stock & Stock::topval(const Stock & s)const{
-    if(s.total_val>total_val)
-        return s;
-    else
-        return *this;
+void Stock::update(double price) {
+  share_val = price;
+  set_tot();
+}
+void Stock::show() const {
+  using std::cout;
+  using std::endl;
+  cout << "Company:" << company << "Shera:" << shares << endl
+       << "Shera Price: $" << share_val << "Total Worth: $" << total_val
+       << endl;
+}
+const Stock &Stock::topval(const Stock &s) const {
+  if (s.total_val > total_val)
+    return s;
+  else
+    return *this;
 }
